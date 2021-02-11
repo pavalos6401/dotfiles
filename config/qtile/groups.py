e7c7e00d5b684c7e3acc65f3b76e438d8e77900a
Pedro Avalos <pavalos6401@gmail.com>
2021-02-10 20:01:59 -0600

    edit groups in qtile

--- config/qtile/groups.py
+++ config/qtile/groups.py
@@ -13,7 +13,7 @@ groups = [
     Group('1', layout='monadtall', label='web'),
     Group('2', layout='monadtall', label='dev'),
     Group('3', layout='monadtall', label='doc'),
-    Group('4', layout='monadtall', label='chat'),
+    Group('4', layout='stack', label='chat'),
     Group('5', layout='monadtall', label='mus'),
     Group('6', layout='max', label='vid'),
     Group('7', layout='tile', label='gam'),
