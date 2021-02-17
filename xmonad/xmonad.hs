92afa5cd59401156bb5fffbe56b39251f0b39378
Pedro Avalos <pavalos6401@gmail.com>
2021-02-16 20:08:16 -0600

    edit xmonad config

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -2,27 +2,37 @@
 -- ~/.xmonad/xmonad.hs
 --
 
--- Base
+-- Inspiration: TTimo xmonad.hs
+-- https://wiki.haskell.org/Xmonad/Config_archive/TTimo_xmonad.hs
+
+-- Imports
 import XMonad
-
--- Dock/Panel/Trays
-import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.ManageHelpers
+import XMonad.Layout.NoBorders
+import XMonad.Layout.Gaps
+import XMonad.Util.Run(spawnPipe)
+import XMonad.Util.EZConfig(additionalKeys)
+import System.IO
 
--- The main function.
-main = xmonad =<< statusBar myBar myPP toggleStrutsKey myConfig
+-- NOTES: 0.10 works better than 0.9 for xmobar
+-- TODO: Would still like fullscreen flash vids to not crop and leave xmobar drawn
+-- TODO: remove the red border when doing fullscreen? `smartBorder` didn't work
+-- TODO: hook in TopicSpaces, start specific apps on specific workspaces
 
--- Command to launch the bar.
-myBar = "xmobar"
-
--- Custom PP. It determines what is being written to the bar
-myPP = xmobarPP { ppCurrent = xmobarColor "#3b4252" "" . wrap "<" ">" }
-
--- Key bindings to toggle the gap for the bar.
-toggleStrutsKey XConfig {XMonad.modMask = modMask} = (modMask, xK_b)
-
--- Main configuration, override the defaults to your liking.
-myConfig = def
-  { modMask  = mod4Mask
-  , terminal = "kitty"
+main = do
+  xmproc <- spawnPipe "/usr/bin/xmobar /home/pavalos/.xmonad/xmobarrc"
+  xmonad defaultConfig {
+    modMask = mod4Mask,
+    terminal = "kitty",
+    -- for 0.9:
+    manageHook = ( isFullscreen --> doFullFloat ) <+> manageDocks <+> manageHook defaultConfig,
+    -- for 0.10:
+    -- manageHook = manageHook <+> manageHook defaultConfig,
+    layoutHook = avoidStruts $ layoutHook defaultConfig,
+    logHook = dynamicLogWithPP $ xmobarPP
+      { ppOutput = hPutStrLn xmproc,
+        ppTitle = xmobarColor "green" "" . shorten 50
+      }
   }
